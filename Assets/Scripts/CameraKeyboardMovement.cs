bc0cc83e883dd3c2b345f8c9d937b8aa8f21cd25
Gabriel Francischini <gpfrancischini@gmail.com>
2015-12-22 10:28:16 -0200

    add ovr camera with keyboard movement

---
+++ Assets/Scripts/CameraKeyboardMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraKeyboardMovement : MonoBehaviour {
+	// Camera movement speed
+	public float cameraSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// var pos = transform.position;
+		// pos.x += Input.GetAxis("Horizontal") * Time.deltaTime * cameraSpeed;
+		// pos.y += Input.GetAxis("Vertical") * Time.deltaTime * cameraSpeed;
+		// transform.position = pos;
+		var euler = transform.eulerAngles;
+		euler.y += Input.GetAxis("Horizontal") * Time.deltaTime * cameraSpeed;
+		euler.x += Input.GetAxis("Vertical") * Time.deltaTime * cameraSpeed;
+		transform.eulerAngles = euler;
+	}
+}
