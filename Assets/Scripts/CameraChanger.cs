786fee6b1b1984ec38de2f56072329f9b88ec8ac
Gabriel Francischini <gpfrancischini@gmail.com>
2015-12-22 10:38:08 -0200

    add camera changer (press return)

---
+++ Assets/Scripts/CameraChanger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraChanger : MonoBehaviour {
+	public Camera camera1;
+	public Camera camera2;
+	// Use this for initialization
+	void Start () {
+		camera1.enabled = true;
+		camera2.enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Return)) {
+			camera1.enabled = !camera1.enabled;
+			camera2.enabled = !camera2.enabled;
+		}
+	}
+}
