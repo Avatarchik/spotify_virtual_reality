316eb490d27fc6b055277f936f0c071c043848e9
Gabriel Francischini <gpfrancischini@gmail.com>
2015-12-29 15:55:36 -0200

    fix some panoramas

--- Assets/Scripts/GlobeControl.cs
+++ Assets/Scripts/GlobeControl.cs
@@ -8,12 +8,17 @@ public class GlobeControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.Rotate(Vector3.up * speed * Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime);
-		if (gameObject.transform.rotation.eulerAngles.y > 0 && gameObject.transform.rotation.eulerAngles.y < 180) {
+		/*if (gameObject.transform.rotation.eulerAngles.y > 0 && gameObject.transform.rotation.eulerAngles.y < 180) {
 			placeControl.setPlace (0);
 			placeControl.playAudio ();
 		} else {
 			placeControl.setPlace (1);
 			placeControl.playAudio ();
-		}
+		}*/
+
+
+		int index = (int)gameObject.transform.rotation.eulerAngles.y / 10;
+		placeControl.setPlace (index);
+		placeControl.playAudio ();
 	}
 }
--- Assets/Scripts/PlaceControl.cs
+++ Assets/Scripts/PlaceControl.cs
@@ -32,15 +32,65 @@ public class PlaceControl : MonoBehaviour {
 
 	// start the audio of the selected index
 	public void playAudio() {
-		if (audioSource.clip != audios [this.index]) {
+		/*if (audioSource.clip != audios [this.index]) {
 			audioSource.clip = audios [this.index];
 			audioSource.Play ();
+		}*/
+	}
+
+	private string getMaterialName(int index) {
+		switch (index) {
+		case 0:
+			return "Boiando";
+		case 1:
+			return "Castelo";
+		case 2:
+			return "Champion_Island";
+		case 3:
+			return "Egito";
+		case 4:
+			return "Elefantinhos";
+		case 5:
+			return "Geleira";
+		case 6:
+			return "Grecia";
+		case 7:
+			return "Iceberg";
+		case 8:
+			return "Isla_Mujeres";
+		case 9:
+			return "Islandia";
+		case 10:
+			return "Natal_Arte";
+		case 11:
+			return "Northern_Lights";
+		case 12:
+			return "original";
+		case 13:
+			return "Pine_Lined_Road";
+		case 14:
+			return "Grecia";
+		case 15:
+			return "Salar";
+		case 16:
+			return "Tartaruguinha";
+		case 17:
+			return "Times";
+		case 18:
+			return "Veneza";
+		case 19:
+			return "Volcano_Hawaii";
+		case 20:
+			return "Yosemite";
 		}
+		return "";
 	}
 
 	// apply the texture of the selected place
 	public void applyMaterial() {
-		GetComponent<Renderer> ().material = mats [this.index];
+		Material material = (Material)Resources.Load("Materials/StreetView/" + getMaterialName(this.index), typeof(Material));
+		GetComponent<Renderer> ().material = material;
+		//GetComponent<Renderer> ().material = mats [this.index];
 	}
 
 }
