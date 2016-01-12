67b1c851ee5d528ef24e176af2cd0bfc7b44d8d2
Gabriel Francischini <gpfrancischini@gmail.com>
2016-01-12 17:54:05 -0200

    change panorama initial position

--- Assets/Scripts/JourneySingleton.cs
+++ Assets/Scripts/JourneySingleton.cs
@@ -8,39 +8,39 @@ public class JourneySingleton : Singleton<JourneySingleton>  {
 	private PinControl currentPin;
 
 	protected JourneySingleton() {
-		addPlace ("Champion_Island", 0);
-		addPlace ("Isla_Mujeres", 3);
-		addPlace ("Times", 14);
-		addPlace ("Salar", 30);
+		addPlace ("Champion_Island", 0, 225);
+		addPlace ("Isla_Mujeres", 3, 199);
+		addPlace ("Times", 14, 325);
+		addPlace ("Salar", 30, 270);
 		addPlace ("Natal_Arte", 55, 270, 10, "Natal", "Natal", "Song", "Artist");
 		//addPlace ("Islandia", 78); -- music missing
-		addPlace ("Castelo", 85);
-		addPlace ("Veneza", 97);
-		addPlace ("Baby_Calf", 98);
-		addPlace ("Pine_Lined_Road", 99);
-		addPlace ("Grecia", 101);
-		addPlace ("Northern_Lights", 102);
-		addPlace ("Egito", 116);
-		addPlace ("Elefantinhos", 122);
-		addPlace ("Castle", 150);
-		addPlace ("Kaindy", 160);
-		addPlace ("India", 173);
-		addPlace ("Ta_Prohm", 194);
-		addPlace ("Montanhas_Laranjas", 209);
-		addPlace ("Cachu_Verdao", 230);
-		addPlace ("Heron_Island", 240);
+		addPlace ("Castelo", 85, 260);
+		addPlace ("Veneza", 97, 180);
+		addPlace ("Baby_Calf", 98, 45);
+		addPlace ("Pine_Lined_Road", 99, 260);
+		addPlace ("Grecia", 101, 0);
+		addPlace ("Northern_Lights", 102, 180);
+		addPlace ("Egito", 116, 335);
+		addPlace ("Elefantinhos", 122, 190);
+		addPlace ("Castle", 150, 0);
+		addPlace ("Kaindy", 160, 200);
+		addPlace ("India", 173, 280);
+		addPlace ("Ta_Prohm", 194, 90);
+		addPlace ("Montanhas_Laranjas", 209, 170);
+		addPlace ("Cachu_Verdao", 230, 180);
+		addPlace ("Heron_Island", 240, 180);
 		//addPlace ("", 245);
 
-		addPlace ("Praia_Com_Estrelas", 250);
-		addPlace ("No_Meio_Da_Floresta", 255);
+		addPlace ("Praia_Com_Estrelas", 250, 0);
+		addPlace ("No_Meio_Da_Floresta", 255, 270);
 
-		addPlace ("Tartaruguinha", 267);
-		addPlace ("Boiando", 271);
+		addPlace ("Tartaruguinha", 267, 145);
+		addPlace ("Boiando", 271, 160);
 
-		addPlace ("Volcano_Hawaii", 298);
+		addPlace ("Volcano_Hawaii", 298, 180);
 
-		addPlace ("Yosemite", 340);
-		addPlace ("Bryce_Canyon", 350);
+		addPlace ("Yosemite", 340, 180);
+		addPlace ("Bryce_Canyon", 350, 235);
 	} // guarantee this will be always a singleton only - can't use the constructor!
 
 	public Place getRandomPlace() {
