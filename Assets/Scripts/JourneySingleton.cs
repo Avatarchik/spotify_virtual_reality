d4d2a4e0a96ef38e86ea606984492806a09b465f
Gabriel Francischini <gpfrancischini@gmail.com>
2016-01-14 15:51:35 -0200

    update places

--- Assets/Scripts/JourneySingleton.cs
+++ Assets/Scripts/JourneySingleton.cs
@@ -22,28 +22,26 @@ public class JourneySingleton : Singleton<JourneySingleton>  {
 		addPlace ("Baby_Calf", 98, 45, defaultTime, "", "Noruega", "", "");
 		addPlace ("Pine_Lined_Road", 99, 260, defaultTime, "Pine-Lined Road", "Slovakia", "Sunny Road", "Emiliana Torrini");
 		addPlace ("Northern_Lights", 101, 180, defaultTime, "Northern Lights", "Rússia", "Aurora", "Runaway");
-
-
 		addPlace ("Grecia", 102, 0, defaultTime, "", "Grécia", "Hanging on", "Ellie Goulding");
 		addPlace ("Egito", 118, 335, defaultTime, "", "Egito", "Walk like an egyptian", "The Bangles");
-		addPlace ("Elefantinhos", 122, 190, defaultTime, "", "", "", "");
-		addPlace ("Castle", 145, 0, defaultTime, "", "", "", "");
-		addPlace ("Kaindy", 152, 200, defaultTime, "", "", "", "");
-		addPlace ("India", 169, 280, defaultTime, "", "", "", "");
-		addPlace ("Ta_Prohm", 194, 90, defaultTime, "", "", "", "");
-		addPlace ("Montanhas_Laranjas", 215, 170, defaultTime, "", "", "", "");
-		addPlace ("Takinoue", 225, defaultTime, "", "", "", "");
-		addPlace ("Cachu_Verdao", 230, 180, defaultTime, "", "", "", "");
-		addPlace ("Heron_Island", 240, 180, defaultTime, "", "", "", "");
-		addPlace ("Praia_Com_Estrelas", 245, 0, defaultTime, "", "", "", "");
-		addPlace ("Pinguins", 250, 245, defaultTime, "", "", "", "");
-		addPlace ("No_Meio_Da_Floresta", 264, 270, defaultTime, "", "", "", "");
-		addPlace ("Tartaruguinha", 267, 145, defaultTime, "", "", "", "");
-		addPlace ("Boiando", 271, 160, defaultTime, "", "", "", "");
-		addPlace ("Volcano_Hawaii", 292, 180, defaultTime, "", "", "", "");
-		addPlace ("Yosemite", 340, 180, defaultTime, "", "", "", "");
-		addPlace ("Bryce_Canyon", 350, 235, defaultTime, "", "", "", "");
-		addPlace ("Champion_Island", 358, 225, defaultTime, "", "", "", "");
+		addPlace ("Elefantinhos", 122, 190, defaultTime, "", "Kenya", "Circle-of-Life", "");
+		addPlace ("Castle", 145, 0, defaultTime, "", "Irão", "", "Bad Girls - M.I.A");
+		addPlace ("Kaindy", 152, 200, defaultTime, "", "Cazaquistão", "Into The Wild", "LP - ");
+		addPlace ("India", 169, 280, defaultTime, "", "India", "", "Mumford & Sons, Laura Marling, & Dharohar Project - Mehendi Rachi ");
+		addPlace ("Ta_Prohm", 194, 90, defaultTime, "", "Camboja", "", "Entrance, washed out (Kripalu Calm)");
+		addPlace ("Montanhas_Laranjas", 215, 170, defaultTime, "", "Austrália", "", "Tame Impala - Elephant ");
+		addPlace ("Takinoue", 225, 0, defaultTime, "", "Japão", "", "Zelda's Lullaby - Taylor Davis");
+		addPlace ("Cachu_Verdao", 230, 180, defaultTime, "", "Tasmânia", "", "Lupa J - Quiet Here ");
+		addPlace ("Heron_Island", 240, 180, defaultTime, "", "Austrália", "", "All Saints - Pure Shores");
+		addPlace ("Praia_Com_Estrelas", 245, 0, defaultTime, "", "", "True to myself ", "Ziggy Marley ");
+		addPlace ("Pinguins", 250, 245, defaultTime, "", "", "I Like To Move It", "Will.I.Am");
+		addPlace ("No_Meio_Da_Floresta", 264, 270, defaultTime, "", "Novazelândia", "", "Jasmine - Jai Paul");
+		addPlace ("Tartaruguinha", 267, 145, defaultTime, "", "Midway Islands", "Moonrise Swing", "La familia de Ukeleles");
+		addPlace ("Boiando", 271, 160, defaultTime, "", "Samoa Americana", "I'm Getting Ready", "Michael Kiwanuka");
+		addPlace ("Volcano_Hawaii", 292, 180, defaultTime, "", "Havaí", "", "Kuana Torres Kahele, Napua Greig, James Ford Murphy - Lava ");
+		addPlace ("Yosemite", 340, 180, defaultTime, "", "USA", "Hello", "Adele ");
+		addPlace ("Bryce_Canyon", 350, 235, defaultTime, "", "USA", "", "Bon Iver - Flume");
+		addPlace ("Champion_Island", 358, 225, defaultTime, "", "", "", " So good at being in trouble - Unknown mortal Orchestra");
 
 	} // guarantee this will be always a singleton only - can't use the constructor!
 
