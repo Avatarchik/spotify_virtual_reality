bd51d40ba06b86fcd718754dfda0828a03c5d754
Gabriel Francischini <gpfrancischini@gmail.com>
2016-01-15 17:52:16 -0200

    add #if to unity editor

--- Assets/StreetViewConverterTools/FetcherAddMenuItem.cs
+++ Assets/StreetViewConverterTools/FetcherAddMenuItem.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 using UnityEditor;
 using System.Collections;
 using ImageFetcherLibrary;
@@ -14,3 +15,4 @@ public class FetcherAddMenuItem
 				window.Show ();
 		}
 }
+#endif
